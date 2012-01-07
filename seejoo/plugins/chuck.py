2b73907a00d827060b415240323c8ce50f5baef2
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2012-01-07 22:22:35 +0100

    Jokes plugin refactored

--- seejoo/plugins/chuck.py
+++ seejoo/plugins/chuck.py
@@ -4,36 +4,34 @@ Created on Jan 7, 2012
 @author: piotr
 '''
 from seejoo.util.common import download
-import json
 from seejoo.ext import plugin, Plugin
+from urllib import urlencode
+import json
+
+
+JOKES_URL = 'http://api.icndb.com/jokes/random'
 
 @plugin
 class Chuck(Plugin):
-    
     commands = { 'j': 'Says facts about Chuck Norris.' }
 
-    def __init__(self):
-        '''
-        Constructor
-        '''
     def command(self, bot, user, cmd, args):
         if cmd != 'j':    return
         
+        url = JOKES_URL
         if args:
-            names = args.split(" ", 2)
-            if(len(names) == 1):
-                jsonData = download("http://api.icndb.com/jokes/random?firstName=" + names[0] + "&lastName=")
-            else:
-                jsonData = download("http://api.icndb.com/jokes/random?firstName=" + names[0] + "&lastName=" + names[1])
-        else:
-            jsonData = download("http://api.icndb.com/jokes/random")
+            names = args.split(" ", 1)
+            url_params = { 'firstName': names[0],
+                           'lastName': names[1] if len(names) > 1 else '' }
+            url += '?' +  urlencode(url_params)
+            
+        jsonData = download(url)
         if not jsonData:
             return "Jokes not available at the moment.";
+
         data = json.loads(jsonData)
         if not data:
             return "Jokes can not be parsed.";
         
-        joke = data["value"]["joke"]
-        if not joke:
-            return "Joke not found"
-        return joke
+        joke = data.get("value", {}).get("joke")
+        return joke or "Joke not found."
