eafcc0fa72bf938650436904bce8583bccab2eb8
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2012-02-20 22:21:39 +0100

    Fixing RSS poll plugin...

--- seejoo/plugins/rss.py
+++ seejoo/plugins/rss.py
@@ -2,13 +2,13 @@
 Plugin for occasional polling of one or more RSS feeds.
 @author Karol Kuczmarski
 '''
-from seejoo.ext import plugin, Plugin, get_storage_dir
+from seejoo.ext import plugin, Plugin
 from seejoo.util import irc
 from datetime import datetime, timedelta
 from lxml import etree
-import urllib
+import time
 import json
-import os
+import logging
 
 
 @plugin
@@ -26,19 +26,17 @@ class Rss(Plugin):
         - name: Some webpage feed
           url: http://somewebpage.com/rss_feed
           frequency: 5 minutes
-          annouce_to: all # default
+          annouce: everywhere # default
         - name: Other webpage feed
           url: http://otherwebpage.com/rss
           frequency: 30 seconds
-          announce_to:
+          announce:
           - one_channel
           - other_channel
     '''
-    DATA_FILE = 'feeds.json'
-
     def __init__(self):
         self.feeds = {}
-        self.data_file = os.path.join(get_storage_dir(self), DATA_FILE)
+        self.state = None
         self.next_poll = datetime.now() # will be in the past for the next tick()
 
     def init(self, bot, config):
@@ -53,24 +51,24 @@ class Rss(Plugin):
                 f['frequency'] = parse_frequency(frequency)
 
         self.feeds = feeds or {}
+        self.state = dict((feed, {}) for feed in self.feeds.keys())
+        print self.feeds
 
     def tick(self, bot):
-        ''' Called every second. Checks the feeds for new items. ''''
+        ''' Called every second. Checks the feeds for new items. '''
+        if not self.state: return
+
         now = datetime.now()
         if self.next_poll > now:
             return
 
-        state = self._read_feeds_state(self)
         global_next_poll = None
-        try:
-            for st in state.iteritems():
-                next_poll = self._poll_and_update_feed(*st)
-                global_next_poll = (next_poll if not feeds_next_poll
-                                    else min(next_poll, global_next_poll))
-        finally:
-            if global_next_poll:
-                self.next_poll = global_next_poll
-            self._save_feeds_state(state)
+        for st in self.state.iteritems():
+            next_poll = self._poll_and_update_feed(*st)
+            global_next_poll = (next_poll if not global_next_poll
+                                else min(next_poll, global_next_poll))
+        if global_next_poll:
+            self.next_poll = global_next_poll
 
 
     def _poll_and_update_feed(self, name, state):
@@ -78,42 +76,36 @@ class Rss(Plugin):
         @return: Time of the next scheduled poll for this feed
         '''
         feed = self.feeds[name]
-        last_poll = datetime.fromtimestamp(st.get('last_poll_time', 0))
+        last_poll = state.get('last_poll_time', datetime.min)
         frequency = feed['frequency']
 
         if last_poll + frequency <= datetime.now():
-            items = poll_rss_feed(feed['url'], st.get('last_item'))
+            logging.debug("Polling RSS feed '%s'...", name)
+            items = poll_rss_feed(feed['url'], state.get('last_item'))
             self._announce_feed(name, items)
-            st['last_poll_time'] = last_poll + frequency
+
+            last_poll = datetime.now()
+            state['last_poll_time'] = last_poll
             if items:
-                st['last_item'] = items[0]['guid']
+                logging.info("Found %s new items in '%s' feed", len(items), name)
+                state['last_item'] = items[0]['guid']
+            else:
+                logging.debug("No new items found in '%s' feed", name)
 
         return last_poll + frequency
 
     def _announce_feed(self, name, items):
         ''' Announces polled feed items to all target channels. '''
         feed = self.feeds[name]
-        channels = feed['announce_to']
-        if channels == 'all':
+        channels = feed.get('announce')
+        if not channels or channels in ['everywhere', 'all']:
             channels = self.bot.channels
 
         for item in items:
-            item_text = "@ %s -> % (by %s) <%s>" % (name, item['title'],
-                                                    item.get('author', 'unknown'), item['link'])
+            item_text = "@ %s -> %s (by %s) <%s>" % (name, item['title'],
+                                                     item.get('author', 'unknown'), item['link'])
             for channel in channels:
                 irc.say(self.bot, channel, item_text)
-
-    def _read_feeds_state(self):
-        ''' Retrieves the state of feeds from data file. '''
-        if not os.path.exists(self.data_file):
-            return {}
-        with open(self.data_file) as f:
-            return json.load(f)
-
-    def _save_feeds_state(self, state):
-        ''' Saves the states of feeds to data file. '''
-        with open(self.data_file, 'w') as f:
-            json.dump(f)
 
 
 ## Utility functions
@@ -154,14 +146,14 @@ def get_rss_items(url):
     @note: All <channel> elements are merged into flat list.
     @return: List of RSS items (dictionaries)
     '''
-    rss = etree.parse(urllib.urlopen(url))
+    rss = etree.parse(url)
     
-    res = {}
-    for channel in rss.iter('channel'):
+    res = []
+    for channel in rss.getroot().iter('channel'):
         for item in channel.iter('item'):
             rss_item = dict((elem.tag, elem.text)
                             for elem in item.iter(tag=etree.Element)
                             if elem.tag != 'item')
             res.append(rss_item)
 
-    return rss
+    return res
