7b87f94623efd0e6a7bd603404fff8d00bbba271
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2012-06-19 21:51:27 +0200

    Some fixes to web.py and setup.py

--- seejoo/commands/web.py
+++ seejoo/commands/web.py
@@ -5,19 +5,19 @@ Created on 2010-12-05
 
 Commands used to access various web services such as Google or Wikipedia.
 '''
+import json
+import re
+import urllib2
+
 from seejoo.ext import command
 from seejoo.util.common import download
 from seejoo.util.strings import strip_html
 from xml.etree import ElementTree
-import json
-import re
-import urllib2
 
 
 MAX_QUERY_RESULTS = 3
 
 
-##############################################################################
 # General commands
 
 @command('rss')
@@ -29,19 +29,16 @@ def get_recent_rss_items(url):
     if not rss:
         return "Could not retrieve RSS feed."
     rss_xml = ElementTree.ElementTree(ElementTree.fromstring(rss))
-    
-    # Get title of channel and titles of items
+
     title = rss_xml._root.find('channel/title').text
     items = [i.text for i in rss_xml._root.findall('channel/item/title')]
     items = items[:MAX_QUERY_RESULTS]
-    
-    # Construct result
+
     res = title + " :: "
     res += " | ".join(items)
     return res
 
 
-###############################################################################
 # Google commands
 
 def _google_websearch(query):
@@ -52,19 +49,23 @@ def _google_websearch(query):
     response = download(url)
     return json.loads(response)
 
+
 def _get_google_websearch_result_count(json_resp):
     '''
     Extracts number of results from Google Web Search JSON response.
     '''
-    if not json_resp:   return
-    try:    
-        result_count = int(json_resp['responseData']['cursor']['estimatedResultCount'])
+    if not json_resp:
+        return
+
+    try:
+        result_count = int(
+            json_resp['responseData']['cursor']['estimatedResultCount'])
     except KeyError:
         return
-    
+
     return result_count
-    
-    
+
+
 @command('g')
 def google_search(query):
     '''
@@ -74,27 +75,31 @@ def google_search(query):
     if not query or len(str(query).strip()) == 0:
         return "No query supplied."
     json_resp = _google_websearch(query)
-    
+
     # Parse the response and return first few results
     results = json_resp['responseData']['results'][:MAX_QUERY_RESULTS]
-    result_count = _get_google_websearch_result_count(json_resp) 
-    
+    result_count = _get_google_websearch_result_count(json_resp)
+
     # Format resulting string
-    res = (r['titleNoFormatting'][:40] + " - " + r['unescapedUrl'] for r in results)
+    res = (r['titleNoFormatting'][:40] + " - " + r['unescapedUrl']
+           for r in results)
     res = "  |  ".join(res)
     if result_count > MAX_QUERY_RESULTS:
         res += " (and about %s more)" % (result_count - MAX_QUERY_RESULTS)
-        
+
     return res
+
 
 @command('gc')
 def google_search_count(query):
     '''
-    Performs a search using Google search engine and returns only the estimated number of results.
+    Performs a search using Google search engine
+    and returns only the estimated number of results.
     '''
     json_resp = _google_websearch(query)
     result_count = _get_google_websearch_result_count(json_resp)
     return "`%s`: about %s results" % (query, result_count)
+
 
 @command('gf')
 def googlefight(queries):
@@ -103,36 +108,40 @@ def googlefight(queries):
     Queries shall be separated with semicolon.
     '''
     if not queries:
-        return "No queries provided."    
+        return "No queries provided."
     queries = map(str.strip, queries.split(";"))
-    
+
     # Query for each term
     results = []
     for query in queries:
         json_resp = _google_websearch(query)
         count = _get_google_websearch_result_count(json_resp)
         results.append((query, count or 0))
-    
+
     # Format results
-    results = sorted(results, key = lambda (_, count): count, reverse = True)
-    results_str = ["%i. %s (%s)" % (place, r[0], r[1]) for place, r in enumerate(results, 1)]
-    
+    results = sorted(results, key=lambda (_, count): count, reverse=True)
+    results_str = ("%i. %s (%s)" % (place, r[0], r[1])
+                   for place, r in enumerate(results, 1))
+
     return " ".join(results_str)
 
 
-###############################################################################
+
 # Wikipedia commands
 
-def get_wikipage_api_url(title, lang = 'en', format = 'json'):
+def get_wikipage_api_url(title, lang='en', format='json'):
     ''' 
     Returns an URL to Wikipedia API call that returns content of given page.
     @note: What is actually returned is the list of most recent revisions
-    with a limit of 1. 
+    with a limit of 1.
     '''
     if not title:
         return None
-    url = "http://%s.wikipedia.org/w/api.php?action=query&prop=revisions&rvlimit=1&rvprop=content&format=%s&titles=%s"
+    url = ("http://%s.wikipedia.org/w/api.php?"
+           "action=query&prop=revisions&rvlimit=1&"
+           "rvprop=content&format=%s&titles=%s")
     return url % (lang, format, urllib2.quote(title))
+
 
 def get_wikipage_content(title):
     '''
@@ -142,66 +151,73 @@ def get_wikipage_content(title):
     resp = download(url)
     if not resp:
         return None
-   
+
     resp = json.loads(resp)
     try:
         pages = resp['query']['pages']
         page = pages.values()[0]
         content = page['revisions'][0].values()[0]
-    except (KeyError, AttributeError), _:
+    except (KeyError, AttributeError):
         return None
-           
+
     return content
 
 
-WIKI_HTML_TAGS = re.compile(r'\<\s*(\w+?)(\s+\w+?\s*\=\s*".*?")*?\s*\>.*?\<\/\s*\1\s*\>')
+WIKI_HTML_TAGS = re.compile(
+    r'\<\s*(\w+?)(\s+\w+?\s*\=\s*".*?")*?\s*\>.*?\<\/\s*\1\s*\>')
 WIKI_HTML_COMMENTS = re.compile(r"\<\!\-\-.*?\-\-\>", re.DOTALL)
 WIKI_UNDERSCORES = re.compile(r"__\w+__")
+
 
 def get_definition_from_wiki(wiki, chars):
     '''
     Retrieves the beginning of definition text from given Wiki page,
     converted from Wiki markup to plaintext.
     '''
-    if not wiki:  return wiki
-    
+    if not wiki:
+        return wiki
+
     # Remove a whole lot of unnecessary stuff (order matters!)
     wiki = WIKI_HTML_COMMENTS.sub("", wiki)
     wiki = WIKI_HTML_TAGS.sub("", wiki)
     wiki = WIKI_UNDERSCORES.sub("", wiki)  # __STUFF_LIKE_THIS___
-    
+
     # Find position of first non-whitespace character on top level of
     # double square or curly brackets
     square_depth = 0
     curly_depth = 0
     for i in xrange(0, len(wiki)):
         c = wiki[i]
-       
-        if c == '[':   square_depth += 1
-        elif c == '{':  curly_depth += 1
-        elif c == ']':  square_depth -= 1
-        elif c == '}':  curly_depth -= 1
+
+        if c == '[':
+            square_depth += 1
+        elif c == '{':
+            curly_depth += 1
+        elif c == ']':
+            square_depth -= 1
+        elif c == '}':
+            curly_depth -= 1
         elif not c.isspace() and (square_depth == curly_depth == 0):
             pos = i
             break
     else:
         return None
     text = wiki[pos:]
-    
+
     # Strip unnecessary markup
     text = re.sub(r"\{\{.*?\}\}", "", text)   # Other remaining markup -- requires refinement to support {{convert...}}
     text = re.sub(r"\[\[(.[^\]]*?\|)?(?P<link>.*?)\]\]",
                   lambda m: m.group("link"), text)     # Links
     text = re.sub(r"\'{3}(.*?)\'{3}",
-                  lambda m: '"%s"' % m.group(1), text)                    # Quotes
-    
+                  lambda m: '"%s"' % m.group(1), text)  # Quotes
+
     text = re.sub(r"\s+\(\W*?\)", "", text)
 
     ending = '...'
     chars -= len(ending)
     return text[:chars] + ending
-    
-        
+
+
 @command('w')
 def wikipedia_definition(term):
     '''
@@ -210,26 +226,25 @@ def wikipedia_definition(term):
     '''
     if not term or len(str(term).strip()) == 0:
         return "No term supplied."
-    
+
     page = get_wikipage_content(term)
     if not page:
         return "Could not connect to Wikipedia."
-    
+
     wiki_def = get_definition_from_wiki(page, 200)
     if not wiki_def:
         return "Could found the definition in Wikipedia."
-    
+
     # Handle Wikipedia redirects
     if wiki_def.startswith("#REDIRECT"):
         first_line = wiki_def[:wiki_def.find('\n')]
         _, target = first_line.split(None, 1)
         return wikipedia_definition(target)
-    
+
     wiki_url = "http://en.wikipedia.org/wiki/" + urllib2.quote(term)
     return "%s -- from: %s" % (wiki_def, wiki_url)
 
 
-##############################################################################
 # Dictionaries
 
 UD_DEF_DIV = re.compile(r'''
@@ -238,19 +253,23 @@ UD_DEF_DIV = re.compile(r'''
     \</div\s*\>                                 # Closing tag
     ''', re.IGNORECASE | re.VERBOSE)
 
-def find_first_sentences(html, s = 1):
+
+def find_first_sentences(html, s=1):
     '''
     Find the first few sentences in given string of HTML.
     '''
     dot = re.compile(r"\.\s+")
-    
+
     pos = i = 0
     while i < s:
-        m = dot.search(html, pos)
-        if not m:   break
-        pos = m.end() ; i += 1
-        
+        match = dot.search(html, pos)
+        if not match:
+            break
+        pos = match.end()
+        i += 1
+
     return html[:pos] if pos > 0 else html
+
 
 @command('ud')
 def urban_dictionary(term):
@@ -260,12 +279,12 @@ def urban_dictionary(term):
     '''
     if not term or len(str(term).strip()) == 0:
         return "No term supplied."
-    
-    url = "http://www.urbandictionary.com/define.php?term=%s" % urllib2.quote(term)
-    ud_site = download(url)
+
+    ud_site = download("http://www.urbandictionary.com/define.php?term=%s" %
+                       urllib2.quote(term))
     if not ud_site:
         return "Could not retrieve definition of '%s'." % term
-    
+
     # Look for definition
     m = UD_DEF_DIV.search(ud_site)
     if m:
@@ -273,11 +292,10 @@ def urban_dictionary(term):
         definition = find_first_sentences(definition, 5)
         if len(definition.strip()) > 0:
             return "'%s': %s" % (term, definition)
-    
+
     return "Could not find definition of '%s'." % term
 
 
-###############################################################################
 # Weather
 
 WEATHER_DIV = re.compile(r'''
@@ -293,6 +311,7 @@ WEATHER_PLACE_DIV = re.compile(r'''
     \</span \s* \> \s* \</div \s*\>                              # Closing tag
     ''', re.IGNORECASE | re.VERBOSE)
 
+
 @command('f')
 def weather_forecast(place):
     '''
@@ -301,29 +320,31 @@ def weather_forecast(place):
     '''
     if not place or len(str(place).strip()) == 0:
         return "No place supplied."
-    
-    url = "http://www.thefuckingweather.com/?zipcode=%s&CELSIUS=yes" % urllib2.quote(place)
-    fw_site = download(url)
-    if not fw_site: return "Could not retrieve weather information."
-    
+
+    fw_site = download(
+        "http://www.thefuckingweather.com/?zipcode=%s&CELSIUS=yes" %
+        urllib2.quote(place))
+    if not fw_site:
+        return "Could not retrieve weather information."
+
     # Look up for the contents of <div class="large">
     m = WEATHER_DIV.search(fw_site)
     if m:
         # Get the contents
         degrees = m.groupdict().get("degrees")
-        comment = m.groupdict().get("comment")        
+        comment = m.groupdict().get("comment")
         if degrees:
-            
             # Try to fetch the actual place
             m = WEATHER_PLACE_DIV.search(fw_site)
-            if m:   place = m.groupdict().get("place", place)
-        
+            if m:
+                place = m.groupdict().get("place", place)
+
             res = "%s: %s^C" % (place, degrees)
             if comment:
                 comment = str(comment).lower()
-                comment = re.sub(r"\<br\s*/?\>", " ", comment) # Convert br's to spaces
+                comment = re.sub(r"\<br\s*/?\>", " ", comment)
                 comment = re.sub(r"\sfucking", "", comment)
                 res += " (%s)" % comment
             return res
-            
+
     return "Could not find weather information."
--- setup.py
+++ setup.py
@@ -9,31 +9,34 @@ Setup script for the seejoo project.
 from setuptools import setup, find_packages
 
 
-setup(name = "seejoo",
-      version = "1.1",
-      description = 'Extensible IRC bot for geek-centered channels',
-      long_description = open("README.markdown").read(),
-      author = 'Karol "Xion" Kuczmarski',
-      author_email = 'karol.kuczmarski@gmail.com',
-      url = 'http://github.com/Xion/seejoo',
-      license = 'Simplified BSD',
-      classifiers = [
-                     'Development Status :: 4 - Beta',
-                     'Environment :: No Input/Output (Daemon)',
-                     'Framework :: Twisted',
-                     'Intended Audience :: Developers',
-                     'Intended Audience :: Information Technology',
-                     'License :: OSI Approved :: BSD License',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 2.6',
-                     'Programming Language :: Python :: 2.7',
-                     'Topic :: Communications :: Chat :: Internet Relay Chat',
-                     'Topic :: Utilities',
-                     ],
-      
-      install_requires = ['twisted', 'lxml'],
-      
-      packages = find_packages(exclude = ['tests']),
-      entry_points = {'console_scripts': ['seejoo=seejoo.main:main']},
+setup(
+    name="seejoo",
+    version="1.1",
+    description='Extensible IRC bot for geek-centered channels',
+    long_description=open("README.markdown").read(),
+    author='Karol "Xion" Kuczmarski',
+    author_email='karol.kuczmarski@gmail.com',
+    url='http://github.com/Xion/seejoo',
+    license='Simplified BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+        'Topic :: Utilities',
+    ],
+
+    install_requires=['twisted', 'lxml'],
+
+    packages=find_packages(exclude=['tests']),
+    entry_points={
+        'console_scripts': ['seejoo=seejoo.main:main']
+    },
 )
