429a8063fe20af0028855be84514fffe21314cf9
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-05-24 20:09:59 +0200

    Allow to specify HTTP headers in .util.common.download

--- seejoo/util/common.py
+++ seejoo/util/common.py
@@ -8,12 +8,25 @@ Contains utility functions that don't fint anywhere else.
 import urllib2
 
 
-def download(url):
-    '''
-    Downloads content of given URL.
-    '''
+def download(url, **headers):
+    """Downloads content of given URL.
+    Additional headers can be passed as keyword arguments.
+    """
+    def header_arg_to_name(header):
+        """Convert header argument (e.g. user_agent) into actual
+        HTTP header name (e.g. User-Agent).
+        """
+        parts = header.split('_')
+        return '-'.join(part.capitalize() for part in parts)
+
+    headers = dict((header_arg_to_name(arg), value)
+                   for arg, value in headers.items())
+    headers.setdefault('User-Agent', "seejoo")
+
     try:
-        req = urllib2.Request(url, headers = { 'User-Agent': 'seejoo'})
+        req = urllib2.Request(url, headers=headers)
         return urllib2.urlopen(req).read()
-    except ValueError:  return download("http://" + url)
-    except IOError:     return None
+    except ValueError:
+        return download("http://" + url, **headers)
+    except IOError:
+        return None
