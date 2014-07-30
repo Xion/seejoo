19c0d329dcc987aed093c42a9d16a3d443292113
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-07-29 21:26:24 -0700

    Remove dubious self-import from setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,79 @@ Created on 2011-08-27
 
 Setup script for the seejoo project.
 '''
-from setuptools import setup, find_packages
+import ast
+import os
+from setuptools import find_packages, setup
 
-import seejoo
+
+def read_tags(filename):
+    """Reads values of "magic tags" defined in the given Python file.
+
+    :param filename: Python filename to read the tags from
+    :return: Dictionary of tags
+    """
+    with open(filename) as f:
+        ast_tree = ast.parse(f.read(), filename)
+
+    res = {}
+    for node in ast.walk(ast_tree):
+        if type(node) is not ast.Assign:
+            continue
+
+        target = node.targets[0]
+        if type(target) is not ast.Name:
+            continue
+
+        if not (target.id.startswith('__') and target.id.endswith('__')):
+            continue
+
+        name = target.id[2:-2]
+        res[name] = ast.literal_eval(node.value)
+
+    return res
+
+
+def read_requirements(filename='requirements.txt'):
+    """Reads the list of requirements from given file.
+
+    :param filename: Filename to read the requirements from.
+    Uses ``'requirements.txt'`` by default.
+
+    :return: Requirements as list of strings
+    """
+    # allow for some leeway with the argument
+    if not filename.startswith('requirements'):
+        filename = 'requirements-' + filename
+    if not os.path.splitext(filename)[1]:
+        filename += '.txt' # no extension, add default
+
+    def valid_line(line):
+        line = line.strip()
+        return line and not any(line.startswith(p) for p in ('#', '-'))
+
+    def extract_requirement(line):
+        egg_eq = '#egg='
+        if egg_eq in line:
+            _, requirement = line.split(egg_eq, 1)
+            return requirement
+        return line
+
+    with open(filename) as f:
+        lines = f.readlines()
+        return list(map(extract_requirement, filter(valid_line, lines)))
+
+
+tags = read_tags(os.path.join('seejoo', '__init__.py'))
 
 
 setup(
     name="seejoo",
-    version=seejoo.__version__,
+    version=tags['version'],
     description='Extensible IRC bot for geek-centered channels',
     long_description=open("README.markdown").read(),
-    author=seejoo.__author__,
+    author=tags['author'],
     url='http://github.com/Xion/seejoo',
-    license=seejoo.__license__,
+    license=tags['license'],
 
     classifiers=[
         'Development Status :: 4 - Beta',
